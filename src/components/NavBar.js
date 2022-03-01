e7fb3b99724ed577f77a87fa1b80ff06abe7baa2
Robert Egeland <robegeland@yahoo.com>
2022-02-28 21:04:56 -0600

    commit tree ec6f731eca812d52ba5d2a2686b0c91621c242e2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linkElements = links.map((nav) => {
+    console.log(nav)
+    let ar = ['#', nav]
+    return <a key={nav} href={ar.join('')}>{nav}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+
+  return <nav>{linkElements}</nav>;
 }
 
 export default NavBar;
